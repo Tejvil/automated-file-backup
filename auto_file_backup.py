469c61b0d0e60554c07a93ebd621c0a00b3044df
TEJVIL CHAVAN <105108433+Tejvil@users.noreply.github.com>
2023-12-08 13:17:04 +0530

    Add files via upload

---
+++ auto_file_backup.py
@@ -0,0 +1,26 @@
+import os
+import shutil   #for copying operations
+import datetime
+import schedule
+import time
+
+source_dir = "C:\\Users\\ASUS\\Videos\\Captures"
+destination_dir ="C:\\Users\\ASUS\\Desktop\\PROJECTS\\py mini projects\\file backup"
+
+def copy_folder_to_directory(source,dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest,str(today))
+    
+    try:
+        shutil.copytree(source,dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f'folder already exist in: {dest}')
+        
+schedule.every().day.at("19:28").do(lambda: copy_folder_to_directory(source_dir,destination_dir))
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+    
+
+
